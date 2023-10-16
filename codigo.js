466ef22f95411d27fa1a593f8988e26b11b46380
Sebastian Castro <sebastiancastro81@gmail.com>
2023-10-16 17:04:54 -0300

    ejercicio dia uno ok

---
+++ codigo.js
@@ -0,0 +1,11 @@
+let numeroUno;
+let numeroDos;
+const titulo = undefined;
+const contenedor = undefined;
+const resultado = undefined;
+var booleanaUno;
+var booleanaDos;
+let alumno = "Sebastian Castro Perez";
+let comision = 23566;
+
+
